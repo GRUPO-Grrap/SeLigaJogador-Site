c8b5a92a4272978ff6089028037689f5b56d8919
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-05-07 18:40:01 -0400

    configuring routes

---
+++ resources/views/events/announce.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.secondary-main')
+
+@section('title', 'Anunciar conta')
+
+@section('contentSecondary')
+
+    <h2>Anunciar</h2>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\AdController;
+
+Route::get('/', [AdController::class, 'index']);
+Route::get('/announce', [AdController::class, 'announce']);
