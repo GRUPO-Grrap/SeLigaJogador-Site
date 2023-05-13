3c21aa199a75491a7a5bc4b105dca6c7f5aaf3d6
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-05-13 19:44:33 -0400

    solving conflict

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,19 +5,23 @@
 @section('content')
 
     <!--Aqui está o header e o navbar-->
-    @include('layouts._partials.navbar')
-
-    <section class="content_top">
-        <div class="content_top_text">
-          <h2 class="content_top_title">O Marketplace para sua conta está aqui!</h2>
-          <h3 class="content_top_subtitle">Venda sua conta com com a gente jogador!</h3>
-          <a href="/events/announce"><button class="content_top_button">Anunciar</button></a>
-        </div>
-    </section>
+    <header>
+        @include('layouts._partials.navbar')
+    </header>
     
-    <section class="carousel">
-
-    </section>
+    <main>
+        <section class="content_top">
+            <div class="content_top_text">
+              <h2 class="content_top_title">O Marketplace para sua conta está aqui!</h2>
+              <h3 class="content_top_subtitle">Venda sua conta com com a gente jogador!</h3>
+              <a href="/events/announce"><button class="content_top_button">Anunciar</button></a>
+            </div>
+        </section>
+    
+        <div class="category">
+            <h2 class="titulo_category">Categorias populares</h2>
+            @include('layouts._partials.category')
+        </div>
 
         <div class="div_main_card ">
             <h2 class="titulo_vendas">Vendas</h2>
