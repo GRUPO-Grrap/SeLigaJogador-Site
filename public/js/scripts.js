674ebc09d0e344ab3646834c0da9ccfc8bd45df2
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-05-14 02:44:19 -0400

    mouse interacting with carousel

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,24 @@
 const carousel = document.querySelector(".carousel");
 
-let isDragStart = false;
+let isDragStart = false, prevPageX, prevScrollLeft;
 
-const dragStart = () => {
+const dragStart = (e) => {
     isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if(!isDragStart) return;
-    carousel.scrollLeft = e.pageX;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
 }
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
--- resources/views/layouts/_partials/category.blade.php
+++ resources/views/layouts/_partials/category.blade.php
@@ -32,11 +32,3 @@
     <img class="img_category" src="/img/clash_of_clans_category.jpg" alt="">
     <p>Clash of clans</p>
 </div>
-
-<div class="card_category">
-    <img class="img_category" src="/img/clash_of_clans_category.jpg" alt="">
-    <p>Clash of clans</p>
-</div>
-
-
-
