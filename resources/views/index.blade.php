e0a1d106edc9b83f14def468052f65cb7a7e9559
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-05-14 11:51:49 -0400

    I UPDATE THE STYLE AND CORRECTED CARD ERROR

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,6 @@
     <!--Aqui está o header e o navbar-->
     <header>
         @include('layouts._partials.navbar')
-    </header>
-    
-    <main>
         <section class="content_top">
             <div class="content_top_text">
               <h2 class="content_top_title">O Marketplace para sua conta está aqui!</h2>
@@ -17,24 +14,26 @@
               <a href="/events/announce"><button class="content_top_button">Anunciar</button></a>
             </div>
         </section>
-        
+    </header>
+    
+    <main>
         <div class="category">
-            <h2 class="titulo_category">Categorias populares</h2>
+            <h2 class="title_category">Categorias populares</h2>
             <div class="carousel">
                 @include('layouts._partials.category')
             </div>
         </div>
 
-        <div>
+        <div class="title_result">
             @if($search)
                 <h3>você buscou por: {{ $search }}</h3>
             @else
-                <h3>as melhores contas aqui!</h3>
+                
             @endif
         </div>
 
         <div class="div_main_card ">
-            <h2 class="titulo_vendas">Vendas</h2>
+            <h2 class="title_vendas">Vendas</h2>
             @include('layouts._partials.card')
             @if(count($adverts) == 0 && $search)
                 <p>Não foi possível encotrar nenhum evento com {{ $search }} <a href="/">Olhar todos!</a> </p>
--- resources/views/layouts/_partials/card.blade.php
+++ resources/views/layouts/_partials/card.blade.php
@@ -5,10 +5,16 @@
          <img class="image_card" src="img/announcement/{{ $advert->image }}" alt="{{ $advert->title }}">
          <span>Conta criada em: {{ date('Y'), strtotime($advert->date) }}</span>
       </div>
+
+      <div class="titulo">
          <h4 class="title_account"> {{$advert->title}} </h4>
-         <p class="card_price"> R$ {{ $advert->preco }} </p>
-         <span class="name_user">User</span>  
       </div>
-          
+         
+
+      <div class="card_price">
+         <p> R$ {{ $advert->preco }} </p> 
+      </div>
+
+      <span class="name_user">User</span>       
    </div>
 @endforeach
