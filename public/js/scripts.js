8eb5304af4ec2abbf8bbdc1c0aea870f8818c773
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-05-15 14:01:09 -0400

    finished carousel

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,14 +4,14 @@ let isDragStart = false, prevPageX, prevScrollLeft;
 
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if(!isDragStart) return;
     e.preventDefault();
-    let positionDiff = e.pageX - prevPageX;
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
@@ -22,3 +22,7 @@ const dragStop = () => {
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("touchup", dragStop);
