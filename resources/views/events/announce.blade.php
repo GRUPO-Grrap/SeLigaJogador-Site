50f4af26f0fcecd7be0d73766fefa37c9352d474
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-05-21 03:10:51 -0400

    Finished styling

--- resources/views/events/announce.blade.php
+++ resources/views/events/announce.blade.php
@@ -11,25 +11,23 @@
 
     <main> 
         <div class="nav-itens">
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                  <a class="nav-link" 
+            <ul class="nav nav-pills mb-4">
+
+                <li class="nav-item mx-1">
+                  <a class="nav-link " aria-current="page" href="#" 
                   style="
-                  background-color: #fafafa;
-                  color: #1a0d42a1;
-                  border: solid 1px #1a0d42a1;" aria-current="page" href="#">Conta</a>
+                  color: #1A0D42;
+                  border: solid .5px #1A0D42">Conta</a>
                 </li>
-              
+
+                <li class="nav-item" 
+                style="
+                background-color: #FF66C4;
+                border: solid 1px #ff51bc"s>
+                  <a class="nav-link" style="color: white" href="#">Anúncio</a>
                 </li>
-                <li class="nav-item" >
-                  <a class="nav-link active" 
-                  style="
-                  background-color: #FF66C4; 
-                  color: white; 
-                  border: solid 1px #FF66C4;" href="#">Anuncio</a>
-                </li>
-            </ul>
-        </div>
+              </ul>
+         </div>
           <div class="announce">
             <div>
                 <form action="/events" method="POST" enctype="multipart/form-data">
@@ -39,9 +37,8 @@
                     <div>
                         <h2><i class="material-icons">campaign</i> Anúncio</h2>
                         <label class="form-label" for="title">Escolha um título para seu anúncio:</label>
-                        <div class="input-group input-group-lg mb-1">
-                            <input type="text" class="form-control" id="title" name="title"
-                            placeholder="Digite aqui o nome do seu anuncio" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+                        <div class="input-group input-group-lg mb-1 mt-1">
+                            <input type="text" class="form-control" id="title" name="title" placeholder="Digite aqui o nome do seu anuncio">
                         </div>
                         <span>Por exemplo: conta Free Fire, rank da sua conta, nome do jogador ou atribuição.</span>
                     </div>
@@ -50,7 +47,7 @@
                     <div class="mt-3">
                         <h2><i class="material-icons">calendar_month</i> Ano de criação da conta</h2>
                         <label for="date">Insira a data de criação da conta</label>
-                        <div  class="input-group input-group-lg mb-1">
+                        <div  class="input-group input-group-lg mb-1 mt-1">
                             <input type="date" class="form-control" id="date" name="date" placeholder="R$0,00">
                         </div>
                     </div>
@@ -59,43 +56,54 @@
                     <div class="mt-3">
                         <h2><i class="material-icons">payments</i> Valor</h2>
                         <label for="preco">Valor do anúncio</label>
-                        <div class="input-group input-group-lg mb-1">
+                        <div class="input-group input-group-lg mb-1 mt-1">
                             <input type="number" class="form-control" id="preco" name="preco" placeholder="R$0,00">
                         </div>
                     </div>
                     
-        
-                    <div>
-                        <h2>Categoria</h2>
+                    
+                    <div class="mt-3">
+                        <h2><i class="material-icons">category</i> Categoria</h2>
                         <label for="category">Escolha a categoria do seu anúncio</label>
-                            <select name="category" id="category">
+                        <div class="input-group input-group-lg mb-1 mt-1">
+                            <select class="form-select" name="category" id="category">
                                 <option value="0">Free Fire</option>
                                 <option value="1">Clash of Clans</option>
                                 <option value="2">Call of Duty</option>
                                 <option value="3">Fifa 23</option>
                             </select>
+                        </div>
                     </div>
-        
-                    <div>
-                        <h2>Descrição</h2>
+                    
+                    
+                    <div class="mt-3">
+                        <h2><i class="material-icons">description</i> Descrição</h2>
                         <label for="description">Drescreva seu anúncio</label>
-                        <span>Adicionar contatospessoais como WhataApp, Discord, Facebook, ou qualquer outro meio de comunicação fará com que o seu anúncio seja aprovado</span>
-                        <textarea name="description" id="description"></textarea>
+                        <div class="input-group input-group-lg mb-1 mt-1">
+                            <textarea class="form-control" name="description" id="description"></textarea>
+                        </div>
+                        <span>Adicionar contatos pessoais como WhataApp, Discord, Facebook, ou qualquer outro meio de comunicação fará com que o seu anúncio seja aprovado</span>
                     </div>
-        
-                    <div>
-                        <label for="image">Imagens</label>
-                        <input type="file" name="image" id="image">
-                        <span>observação</span>
+                    
+                    <div class="mt-3">
+                        <h2 for="image"><i class="material-icons">image</i> Imagens</h2>
+                        <div  class="input-group input-group-lg mb-1 mt-1">
+                            <input class="form-control"  type="file" name="image" id="image">
+                        </div>
+                        <span>Imagens que não tiverem relção com os anuncio, serão penalizadas</span>
                     </div>
-        
-                    <div>
-                        <h2>Contrato</h2>
-                        <p>Aqui é o Contrato</p>
-                        <input type="checkbox"  placeholder="R$0,00">
+                   
+                    <div class="mt-3">
+                        <h2><i class="material-icons">article</i> Termos de uso</h2>
+                        <div class="form-check"> 
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <p class="form-check-label" for="flexCheckDefault">
+                                Eu li e concordo com os <a href="#">Termos de uso</a> da site Se Liga Jogador
+                            </p>
+                        </div>
                     </div>
-        
-                    <button type="submit">Finalizar</button>
+                
+                    <button class="btn_send" type="submit">Finalizar</button>
                 </form>
             </div>
         </div>
